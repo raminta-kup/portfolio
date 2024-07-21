f659d97ffbf0f7d10d355f3889c810ea744851b5
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2024-07-21 20:38:43 +0300

    created MainContainer, added reset css

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,23 @@
+
+
+export const MainContainer = () => {
+    return (
+        <header>
+            <div>
+                <ul>
+                    <li>html</li>
+                    <li>css</li>
+                    <li>javascript</li>
+                    <li>react</li>
+                </ul>
+                <ul>
+                    <li>email</li>
+                    <li>github</li>
+                </ul>
+            </div>
+            <h1>
+                hi. my name is raminta and i am a front end developer
+            </h1>
+        </header>
+    )
+}
